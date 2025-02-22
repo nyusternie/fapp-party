1783e13295e3d113af29f68e5c010c5913aecd69
Shomari <nyusternie@sdot.io>
2025-02-22 00:13:57 -0500

    Add new contracts.

---
+++ contracts/WarpletPartyPurse.sol
@@ -0,0 +1,21 @@
+/*******************************************************************************
+ *
+ * Copyright (c) 2025 Build On Farcaster DAO.
+ * SPDX-License-Identifier: MIT
+ *
+ * WarpletPartyPurse - TBD.
+ *
+ * Version 1 (alpha)
+ * Released 25.2.22
+ *
+ * https://warplet.party
+ * https://buildonfarcaster.org
+ */
+pragma solidity ^0.8.9;
+
+import { Ownable } from "../interfaces/Ownable.sol";
+import { IERC20 } from "../interfaces/IERC20.sol";
+
+contract WarpletPartyPurse is Ownable {
+    // TODO
+}
