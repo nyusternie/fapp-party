5d8033c78b9371a2a51f64e4cda2a2a5f6fd12fb
Shomari <nyusternie@sdot.io>
2025-06-02 17:32:20 -0400

    Update roadmap.

---
+++ web/src/components/MiniAppFooter.vue
@@ -0,0 +1,12 @@
+<!-- ---
+import astroLogo from '../assets/astro.svg'
+import icon from '../assets/icon.svg'
+--- -->
+
+<template>
+    <footer class="bg-gray-900">
+        <div class="text-rose-500 font-bold text-6xl">
+            MINI APP FOOTER
+        </div>
+    </footer>
+</template>
---
+++ web/src/components/MiniAppHeader.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+/* Define properties. */
+// https://vuejs.org/guide/components/props.html#props-declaration
+const props = defineProps({
+    data: {
+        type: [Object],
+    },
+})
+
+const isShowingMobileMenu = ref(false)
+
+// onMounted(() => {
+//     console.log('Mounted!')
+//     // Now it's safe to perform setup operations.
+// })
+
+// onBeforeUnmount(() => {
+//     console.log('Before Unmount!')
+//     // Now is the time to perform all cleanup operations.
+// })
+</script>
+
+<template>
+    <header class="absolute inset-x-0 top-0 z-50">
+        <div class="text-rose-500 font-bold text-6xl">
+            MINI APP HEADER
+        </div>
+    </header>
+</template>
