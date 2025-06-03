ce71e68485dc6e8d6f52d9e0d1521072b19d5fe2
Shomari <nyusternie@sdot.io>
2025-06-02 20:11:22 -0400

    Test init.

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,15 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from '@tailwindcss/vite'
 
-import vue from '@astrojs/vue';
+import vue from '@astrojs/vue'
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  },
+    vite: {
+        plugins: [tailwindcss()]
+    },
 
-  integrations: [vue()]
-});
+    integrations: [vue()]
+})
