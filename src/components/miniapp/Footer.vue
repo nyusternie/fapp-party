0e53e72931b63e54e30af3770126a3d7b394d886
Shomari <nyusternie@sdot.io>
2025-06-08 17:27:07 -0400

    Update UI.

--- src/components/miniapp/Footer.vue
+++ src/components/miniapp/Footer.vue
@@ -1,19 +1,19 @@
 <template>
     <footer class="w-full h-[60px] grid grid-cols-3 gap-0.5 justify-between bg-slate-900 overflow-hidden">
         <a href="/profile" class="group w-full flex justify-center items-center bg-amber-200 hover:bg-amber-700">
-            <span class="text-amber-700 font-bold text-xl tracking-wider uppercase group-hover:text-amber-200">
+            <span class="text-amber-700 font-bold text-lg tracking-wider uppercase group-hover:text-amber-200">
                 Profile
             </span>
         </a>
 
         <a href="/sponsors" class="group w-full flex justify-center items-center bg-amber-200 hover:bg-amber-700">
-            <span class="text-amber-700 font-bold text-xl tracking-wider uppercase group-hover:text-amber-200">
+            <span class="text-amber-700 font-bold text-lg tracking-wider uppercase group-hover:text-amber-200">
                 Sponsors
             </span>
         </a>
 
         <a href="/spotlight" class="group w-full flex justify-center items-center bg-amber-200 hover:bg-amber-700">
-            <span class="text-amber-700 font-bold text-xl tracking-wider uppercase group-hover:text-amber-200">
+            <span class="text-amber-700 font-bold text-lg tracking-wider uppercase group-hover:text-amber-200">
                 Spotlight
             </span>
         </a>
