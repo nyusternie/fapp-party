a9b576292a93b9d6ae2bc7ecd5ceb691e2d8dafb
Shomari <nyusternie@sdot.io>
2025-06-08 17:25:41 -0400

    Reduce font size.

--- src/components/miniapp/Footer.vue
+++ src/components/miniapp/Footer.vue
@@ -1,20 +1,20 @@
 <template>
-    <footer class="w-full h-[60px] flex flex-row gap-0.5 justify-between bg-slate-900 overflow-hidden">
+    <footer class="w-full h-[60px] grid grid-cols-3 gap-0.5 justify-between bg-slate-900 overflow-hidden">
         <a href="/profile" class="group w-full flex justify-center items-center bg-amber-200 hover:bg-amber-700">
-            <span class="text-amber-700 font-bold text-2xl tracking-wider group-hover:text-amber-200">
-                PROFILE
+            <span class="text-amber-700 font-bold text-xl tracking-wider uppercase group-hover:text-amber-200">
+                Profile
             </span>
         </a>
 
         <a href="/sponsors" class="group w-full flex justify-center items-center bg-amber-200 hover:bg-amber-700">
-            <span class="text-amber-700 font-bold text-2xl tracking-wider group-hover:text-amber-200">
-                SPONSORS
+            <span class="text-amber-700 font-bold text-xl tracking-wider uppercase group-hover:text-amber-200">
+                Sponsors
             </span>
         </a>
 
         <a href="/spotlight" class="group w-full flex justify-center items-center bg-amber-200 hover:bg-amber-700">
-            <span class="text-amber-700 font-bold text-2xl tracking-wider group-hover:text-amber-200">
-                SPOTLIGHT
+            <span class="text-amber-700 font-bold text-xl tracking-wider uppercase group-hover:text-amber-200">
+                Spotlight
             </span>
         </a>
     </footer>
