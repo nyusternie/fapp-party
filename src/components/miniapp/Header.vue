52b1bb32feab562d630e3de13874682956912cda
Shomari <nyusternie@sdot.io>
2025-06-08 18:09:19 -0400

    Add notifications page.

--- src/components/miniapp/Header.vue
+++ src/components/miniapp/Header.vue
@@ -1,11 +1,17 @@
 <template>
     <header class="w-full h-[60px] flex flex-row gap-3 justify-between bg-slate-900 overflow-hidden">
         <div class="w-full h-full flex justify-center items-center border-4 border-rose-500">
-            <h1 class="text-sky-700 font-bold text-3xl">
-                MINI APP HEADER
+            <h1 class="text-sky-200 font-bold text-2xl">
+                PARTY CENTER
             </h1>
 
         </div>
+
+        <a href="/notif" class="group w-[75px] flex justify-center items-center bg-amber-200 hover:bg-amber-700">
+            <svg class="p-3 text-amber-700 font-bold text-lg tracking-wider uppercase group-hover:text-amber-200" data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0M3.124 7.5A8.969 8.969 0 0 1 5.292 3m13.416 0a8.969 8.969 0 0 1 2.168 4.5"></path>
+            </svg>
+        </a>
 
         <a href="/profile" class="group w-[75px] flex justify-center items-center bg-amber-200 hover:bg-amber-700">
             <svg class="p-3 text-amber-700 font-bold text-lg tracking-wider uppercase group-hover:text-amber-200" data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
