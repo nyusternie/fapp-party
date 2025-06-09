25fde94bc921c8d0505b279543140808ffa01347
Shomari <nyusternie@sdot.io>
2025-06-09 14:03:23 -0400

    Link to roadmap.

--- src/components/miniapp/Header.vue
+++ src/components/miniapp/Header.vue
@@ -1,14 +1,13 @@
 <template>
     <header class="w-full h-[60px] p-2 flex flex-row gap-3 justify-between bg-slate-900 overflow-hidden">
-        <div class="w-full h-full flex justify-center items-center border border-red-300/70 rounded-lg">
+        <a href="/roadmap" class="w-full h-full flex justify-center items-center border border-red-300/70 rounded-lg">
             <h1 class="inline-flex items-center gap-2 text-red-300/70 font-bold text-2xl tracking-tighter italic">
                 <svg class="size-6" data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126ZM12 15.75h.007v.008H12v-.008Z"></path>
                 </svg>
                 EARLY PREVIEW
             </h1>
-
-        </div>
+        </a>
 
         <a href="/notif">
             <svg class="h-full p-2 bg-stone-300 text-stone-700 font-bold text-lg rounded-full tracking-wider hover:text-stone-200 hover:bg-stone-700" data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
