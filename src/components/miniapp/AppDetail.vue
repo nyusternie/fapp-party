6b2ba5d78678319c65d91069665e2d8866f8e123
Shomari <nyusternie@sdot.io>
2025-06-09 18:05:38 -0400

    Update button.

--- src/components/miniapp/AppDetail.vue
+++ src/components/miniapp/AppDetail.vue
@@ -19,10 +19,10 @@
             </div>
 
             <button class="group cursor-pointer w-full px-5 py-3 flex justify-center items-center bg-blue-800 border-2 hover:bg-blue-400 border-amber-900 rounded-xl" @click="openUrl(props.appid)">
-                <span class="inline-flex items-center gap-3 font-bold text-2xl uppercase text-blue-300 group-hover:text-blue-800">
+                <span class="inline-flex items-center gap-3 font-bold text-2xl uppercase text-amber-300 group-hover:text-amber-200">
                     Launch Mini App
 
-                    <svg class="size-5 text-amber-600 group-hover:text-amber-200" data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                    <svg class="size-5 text-amber-400 group-hover:text-amber-200" data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"></path>
                     </svg>
                 </span>
