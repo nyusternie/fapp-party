8bf32cf73430ab628f0f158b59c7c32907479247
Shomari <nyusternie@sdot.io>
2025-06-09 13:34:15 -0400

    Update UI (for all primary pages).

--- src/components/miniapp/Help.vue
+++ src/components/miniapp/Help.vue
@@ -1,12 +1,26 @@
 <template>
     <main class="w-full h-full px-2 pb-3 flex flex-col gap-6 overflow-y-scroll">
 		<h1 class="mt-10 text-pretty text-5xl font-semibold tracking-tight text-fuchsia-700 sm:text-7xl">
-			Search
+			Help Center
 		</h1>
 
-		<p>
-			These are community SPONSORED Mini Apps that fund the Party Treasury—the primary funding source for ALL <a href="/rewards" class="text-lg font-bold text-blue-500 hover:text-blue-400 hover:underline">Reward Pools</a> and community events.
+		<p class="text-slate-300 font-medium text-lg">
+			Need help?
+			Reach out directly to
+			<button @click="openShomari" class="group cursor-pointer px-2 py-0 inline-flex flex-col items-center text-sky-800 bg-sky-300 border border-sky-500 rounded-xl hover:bg-sky-900 hover:text-sky-100">
+				shomari.eth
+				<span class="-mt-2 block text-xs text-sky-700 tracking-widest group-hover:text-sky-200">BDFL</span>
+			</button>
+			for immediate assistance.
 		</p>
+
+		<ul class="pl-10 list-disc">
+			<li>
+				<a href="/roadmap" class="text-xl font-medium text-amber-200 tracking-wider hover:text-amber-100 hover:underline">
+					View Our Official Roadmap
+				</a>
+			</li>
+		</ul>
 
 		<!-- <section>
 			<h3>
@@ -26,6 +40,7 @@
 /* Import modules. */
 import { onMounted, ref } from 'vue'
 import { useStore } from '@nanostores/vue'
+import { sdk } from '@farcaster/frame-sdk'
 
 import $System from '../../stores/system'
 
@@ -41,6 +56,14 @@ const System = useStore($System)
 
 const init = async () => {
     console.log('SYSTEM', $System.get())
+}
+
+const openShomari = async () => {
+	/* Set FID. */
+	const fid = 870594
+
+	/* Open profile. */
+	await sdk.actions.viewProfile({ fid })
 }
 
 onMounted(() => {
---
+++ src/components/miniapp/Notif.vue
@@ -0,0 +1,78 @@
+<template>
+    <main class="w-full h-full px-2 pb-3 flex flex-col gap-6 overflow-y-scroll">
+		<h1 class="mt-10 text-pretty text-5xl font-semibold tracking-tight text-fuchsia-700 sm:text-7xl">
+			Notifications
+		</h1>
+
+		<div>
+			<h2 class="text-slate-100 font-bold text-2xl tracking-wider">
+				FOMO (Real-time) Updates
+			</h2>
+
+			<p class="text-slate-300 font-medium text-lg">
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse vel voluptates odit provident consectetur consequatur dolore obcaecati sequi, iure quia!
+			</p>
+		</div>
+
+		<div>
+			<h2 class="text-slate-100 font-bold text-2xl tracking-wider">
+				Daily (New Arrival) Updates
+			</h2>
+
+			<p class="text-slate-300 font-medium text-lg">
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse vel voluptates odit provident consectetur consequatur dolore obcaecati sequi, iure quia!
+			</p>
+		</div>
+
+		<div>
+			<h2 class="text-slate-100 font-bold text-2xl tracking-wider">
+				Weekly (Reward Pool) Updates
+			</h2>
+
+			<p class="text-slate-300 font-medium text-lg">
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse vel voluptates odit provident consectetur consequatur dolore obcaecati sequi, iure quia!
+			</p>
+		</div>
+
+		<div>
+			<h2 class="text-slate-100 font-bold text-2xl tracking-wider">
+				Seasonal (Event &amp; Competition) Updates
+			</h2>
+
+			<p class="text-slate-300 font-medium text-lg">
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse vel voluptates odit provident consectetur consequatur dolore obcaecati sequi, iure quia!
+			</p>
+		</div>
+	</main>
+</template>
+
+<script setup lang="ts">
+/* Import modules. */
+import { onMounted, ref } from 'vue'
+import { useStore } from '@nanostores/vue'
+
+import $System from '../../stores/system'
+
+/* Define properties. */
+// https://vuejs.org/guide/components/props.html#props-declaration
+const props = defineProps({
+    data: {
+        type: [Object],
+    },
+})
+
+const System = useStore($System)
+
+const init = async () => {
+    console.log('SYSTEM', $System.get())
+}
+
+onMounted(() => {
+    init()
+})
+
+// onBeforeUnmount(() => {
+//     console.log('Before Unmount!')
+//     // Now is the time to perform all cleanup operations.
+// })
+</script>
