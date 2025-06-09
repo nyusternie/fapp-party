557568ac7839d9414b2699197ce08b0b2b3014aa
Shomari <nyusternie@sdot.io>
2025-06-09 06:00:01 -0400

    Update footer.

--- src/components/miniapp/Footer.vue
+++ src/components/miniapp/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-    <footer class="w-full h-[45px] grid grid-cols-4 gap-0.5 justify-between bg-slate-900 overflow-hidden">
+    <footer class="w-full h-[45px] grid grid-cols-4 gap-0.5 justify-between bg-slate-900 border border-t-stone-700 overflow-hidden">
         <a href="/apps" class="group w-full flex justify-center items-center bg-stone-200 hover:bg-stone-700">
             <svg class="size-8 text-stone-700 font-bold text-lg tracking-wider uppercase group-hover:text-stone-200" data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M15.59 14.37a6 6 0 0 1-5.84 7.38v-4.8m5.84-2.58a14.98 14.98 0 0 0 6.16-12.12A14.98 14.98 0 0 0 9.631 8.41m5.96 5.96a14.926 14.926 0 0 1-5.841 2.58m-.119-8.54a6 6 0 0 0-7.381 5.84h4.8m2.581-5.84a14.927 14.927 0 0 0-2.58 5.84m2.699 2.7c-.103.021-.207.041-.311.06a15.09 15.09 0 0 1-2.448-2.448 14.9 14.9 0 0 1 .06-.312m-2.24 2.39a4.493 4.493 0 0 0-1.757 4.306 4.493 4.493 0 0 0 4.306-1.758M16.5 9a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Z"></path>
