23e341acacad1643036629c6d6858d97e8d5569f
Shomari <nyusternie@sdot.io>
2025-06-09 05:33:58 -0400

    Update nav links.

--- src/components/miniapp/Apps.vue
+++ src/components/miniapp/Apps.vue
@@ -94,7 +94,7 @@
 
             <section class="mt-5 w-full grid grid-cols-2 gap-0.5">
                 <a href="#" class="group py-2 flex justify-center items-center bg-amber-100 rounded-lg rounded-r-none hover:bg-amber-600">
-                    <span span class="flex flex-row items-center gap-2 text-amber-500 font-bold text-2xl group-hover:text-amber-50">
+                    <span span class="flex flex-row items-center gap-2 text-amber-500 font-bold text-2xl uppercase group-hover:text-amber-50">
                         <svg class="size-6" data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                             <path stroke-linecap="round" stroke-linejoin="round" d="m11.25 9-3 3m0 0 3 3m-3-3h7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"></path>
                         </svg>
@@ -104,7 +104,7 @@
                 </a>
 
                 <a href="#" class="group py-2 flex flex-row justify-center items-center bg-amber-100 rounded-lg rounded-l-none hover:bg-amber-600">
-                    <span span class="flex flex-row items-center gap-2 text-amber-500 font-bold text-2xl group-hover:text-amber-50">
+                    <span span class="flex flex-row items-center gap-2 text-amber-500 font-bold text-2xl uppercase group-hover:text-amber-50">
                         Older
 
                         <svg class="size-6" data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
--- src/components/miniapp/Header.vue
+++ src/components/miniapp/Header.vue
@@ -1,5 +1,5 @@
 <template>
-    <header class="w-full h-[60px] p-2 flex flex-row gap-2 justify-between bg-slate-900 overflow-hidden">
+    <header class="w-full h-[60px] p-2 flex flex-row gap-3 justify-between bg-slate-900 overflow-hidden">
         <div class="w-full h-full flex justify-center items-center border border-amber-200 rounded-lg">
             <h1 class="text-amber-200 font-bold text-2xl">
                 PARTY CENTER
@@ -8,14 +8,14 @@
         </div>
 
         <a href="/notif">
-            <svg class="h-full p-2 bg-stone-300 text-stone-700 font-bold text-lg rounded-full tracking-wider group-hover:text-stone-200" data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+            <svg class="h-full p-2 bg-stone-300 text-stone-700 font-bold text-lg rounded-full tracking-wider hover:text-stone-200 hover:bg-stone-700" data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0M3.124 7.5A8.969 8.969 0 0 1 5.292 3m13.416 0a8.969 8.969 0 0 1 2.168 4.5"></path>
             </svg>
         </a>
 
         <a href="/profile">
-            <img v-if="pfpUrl" :src="pfpUrl" class="h-full w-[60px] object-cover rounded-full border-2 border-stone-300" />
-            <svg v-else class="h-full p-2 bg-stone-300 text-stone-700 font-bold text-lg rounded-full tracking-wider group-hover:text-stone-200" data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+            <img v-if="pfpUrl" :src="pfpUrl" class="h-full w-[60px] object-cover rounded-full border-2 border-stone-300 hover:border-amber-300" />
+            <svg v-else class="h-full p-2 bg-stone-300 text-stone-700 font-bold text-lg rounded-full tracking-wider hover:text-stone-200 hover:bg-stone-700" data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"></path>
             </svg>
         </a>
