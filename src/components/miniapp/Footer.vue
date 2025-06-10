cf08411e1e28507410a83004ad72377e87745037
Shomari <nyusternie@sdot.io>
2025-06-10 00:25:55 -0400

    Invert footer colors.

--- src/components/miniapp/Footer.vue
+++ src/components/miniapp/Footer.vue
@@ -1,26 +1,26 @@
 <template>
     <footer class="w-full h-[45px] grid grid-cols-4 gap-0.5 justify-between bg-slate-900 border border-t-stone-700 overflow-hidden">
-        <a href="/apps" class="group w-full flex justify-center items-center bg-stone-200 hover:bg-stone-700">
-            <svg class="size-8 text-amber-700 font-bold text-lg tracking-wider uppercase group-hover:text-amber-200" data-slot="icon" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+        <a href="/apps" class="group w-full flex justify-center items-center bg-stone-700 hover:bg-stone-200">
+            <svg class="size-8 text-amber-200 font-bold text-lg tracking-wider uppercase group-hover:text-amber-700" data-slot="icon" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                 <path d="M5.127 3.502 5.25 3.5h9.5c.041 0 .082 0 .123.002A2.251 2.251 0 0 0 12.75 2h-5.5a2.25 2.25 0 0 0-2.123 1.502ZM1 10.25A2.25 2.25 0 0 1 3.25 8h13.5A2.25 2.25 0 0 1 19 10.25v5.5A2.25 2.25 0 0 1 16.75 18H3.25A2.25 2.25 0 0 1 1 15.75v-5.5ZM3.25 6.5c-.04 0-.082 0-.123.002A2.25 2.25 0 0 1 5.25 5h9.5c.98 0 1.814.627 2.123 1.502a3.819 3.819 0 0 0-.123-.002H3.25Z"></path>
             </svg>
         </a>
 
-        <a href="/search" class="group w-full flex justify-center items-center bg-stone-200 hover:bg-stone-700">
-            <svg class="size-8 text-amber-700 font-bold text-lg tracking-wider uppercase group-hover:text-amber-200" data-slot="icon" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+        <a href="/search" class="group w-full flex justify-center items-center bg-stone-700 hover:bg-stone-200">
+            <svg class="size-8 text-amber-200 font-bold text-lg tracking-wider uppercase group-hover:text-amber-700" data-slot="icon" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                 <path d="M6.5 9a2.5 2.5 0 1 1 5 0 2.5 2.5 0 0 1-5 0Z"></path>
                 <path clip-rule="evenodd" fill-rule="evenodd" d="M10 18a8 8 0 1 0 0-16 8 8 0 0 0 0 16ZM9 5a4 4 0 1 0 2.248 7.309l1.472 1.471a.75.75 0 1 0 1.06-1.06l-1.471-1.472A4 4 0 0 0 9 5Z"></path>
             </svg>
         </a>
 
-        <a href="/rewards" class="group w-full flex justify-center items-center bg-stone-200 hover:bg-stone-700">
-            <svg class="size-8 text-amber-700 font-bold text-lg tracking-wider uppercase group-hover:text-amber-200" data-slot="icon" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+        <a href="/rewards" class="group w-full flex justify-center items-center bg-stone-700 hover:bg-stone-200">
+            <svg class="size-8 text-amber-200 font-bold text-lg tracking-wider uppercase group-hover:text-amber-700" data-slot="icon" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                 <path clip-rule="evenodd" fill-rule="evenodd" d="M10 1c-1.828 0-3.623.149-5.371.435a.75.75 0 0 0-.629.74v.387c-.827.157-1.642.345-2.445.564a.75.75 0 0 0-.552.698 5 5 0 0 0 4.503 5.152 6 6 0 0 0 2.946 1.822A6.451 6.451 0 0 1 7.768 13H7.5A1.5 1.5 0 0 0 6 14.5V17h-.75C4.56 17 4 17.56 4 18.25c0 .414.336.75.75.75h10.5a.75.75 0 0 0 .75-.75c0-.69-.56-1.25-1.25-1.25H14v-2.5a1.5 1.5 0 0 0-1.5-1.5h-.268a6.453 6.453 0 0 1-.684-2.202 6 6 0 0 0 2.946-1.822 5 5 0 0 0 4.503-5.152.75.75 0 0 0-.552-.698A31.804 31.804 0 0 0 16 2.562v-.387a.75.75 0 0 0-.629-.74A33.227 33.227 0 0 0 10 1ZM2.525 4.422C3.012 4.3 3.504 4.19 4 4.09V5c0 .74.134 1.448.38 2.103a3.503 3.503 0 0 1-1.855-2.68Zm14.95 0a3.503 3.503 0 0 1-1.854 2.68C15.866 6.449 16 5.74 16 5v-.91c.496.099.988.21 1.475.332Z"></path>
             </svg>
         </a>
 
-        <a href="/help" class="group w-full flex justify-center items-center bg-stone-200 hover:bg-stone-700">
-            <svg class="size-8 text-amber-700 font-bold text-lg tracking-wider uppercase group-hover:text-amber-200" data-slot="icon" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+        <a href="/help" class="group w-full flex justify-center items-center bg-stone-700 hover:bg-stone-200">
+            <svg class="size-8 text-amber-200 font-bold text-lg tracking-wider uppercase group-hover:text-amber-700" data-slot="icon" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                 <path clip-rule="evenodd" fill-rule="evenodd" d="M18 10a8 8 0 1 1-16 0 8 8 0 0 1 16 0Zm-7-4a1 1 0 1 1-2 0 1 1 0 0 1 2 0ZM9 9a.75.75 0 0 0 0 1.5h.253a.25.25 0 0 1 .244.304l-.459 2.066A1.75 1.75 0 0 0 10.747 15H11a.75.75 0 0 0 0-1.5h-.253a.25.25 0 0 1-.244-.304l.459-2.066A1.75 1.75 0 0 0 9.253 9H9Z"></path>
             </svg>
         </a>
