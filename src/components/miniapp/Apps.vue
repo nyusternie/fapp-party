2830b720c5471d44cd2f9d238a1f8c7f6a4cab87
Shomari <nyusternie@sdot.io>
2025-06-09 21:52:16 -0400

    Enable pagination.

--- src/components/miniapp/Apps.vue
+++ src/components/miniapp/Apps.vue
@@ -124,8 +124,8 @@
 <!-- END BELOW THE FOLD -->
 
             <section class="my-5 w-full px-10 grid grid-cols-2 gap-0.5">
-                <a href="#" class="opacity-30 cursor-not-allowed group py-2 flex justify-center items-center bg-amber-100 rounded-lg rounded-r-none">
-                    <span span class="flex flex-row items-center gap-2 text-amber-500 font-bold text-3xl uppercase">
+                <a href="#" class="opacity-30 cursor-not-allowed group px-5 py-2 flex justify-center items-center bg-amber-100 rounded-lg rounded-r-none">
+                    <span span class="flex flex-row items-center gap-2 text-amber-500 font-bold text-2xl uppercase">
                         <svg class="size-6" data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                             <path stroke-linecap="round" stroke-linejoin="round" d="m11.25 9-3 3m0 0 3 3m-3-3h7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"></path>
                         </svg>
@@ -134,8 +134,8 @@
                     </span>
                 </a>
 
-                <a href="#" class="group py-2 flex flex-row justify-center items-center bg-amber-100 rounded-lg rounded-l-none hover:bg-amber-600">
-                    <span span class="flex flex-row items-center gap-2 text-amber-500 font-bold text-3xl uppercase group-hover:text-amber-50">
+                <a href="/apps/page2" class="group px-5 py-2 flex flex-row justify-center items-center bg-amber-100 rounded-lg rounded-l-none hover:bg-amber-600">
+                    <span span class="flex flex-row items-center gap-2 text-amber-500 font-bold text-2xl uppercase group-hover:text-amber-50">
                         Older
 
                         <svg class="size-6" data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
