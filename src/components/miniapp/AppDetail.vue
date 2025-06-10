b1d32e698847642c3c79888ca6e4a94d6f134dca
Shomari <nyusternie@sdot.io>
2025-06-10 19:05:43 -0400

    Fix button.

--- src/components/miniapp/AppDetail.vue
+++ src/components/miniapp/AppDetail.vue
@@ -115,7 +115,7 @@
             </div>
 
             <a :href="'/boost/' + appDetails.hostname" class="group cursor-pointer w-full mt-2 px-5 py-3 flex flex-col justify-center items-center bg-fuchsia-800 border-2 hover:bg-fuchsia-200 border-amber-900 rounded-xl">
-                <span class="inline-flex items-center gap-2 font-bold text-2xl text-center uppercase text-amber-300 group-hover:text-fuchsia-800">
+                <span class="inline-flex line-clamp-1 items-center gap-2 font-bold text-2xl text-center uppercase text-amber-300 group-hover:text-fuchsia-800">
                     Boost
                     <svg class="size-8 text-fuchsia-200 group-hover:text-fuchsia-800" data-slot="icon" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                         <path clip-rule="evenodd" fill-rule="evenodd" d="M4.606 12.97a.75.75 0 0 1-.134 1.051 2.494 2.494 0 0 0-.93 2.437 2.494 2.494 0 0 0 2.437-.93.75.75 0 1 1 1.186.918 3.995 3.995 0 0 1-4.482 1.332.75.75 0 0 1-.461-.461 3.994 3.994 0 0 1 1.332-4.482.75.75 0 0 1 1.052.134Z"></path>
