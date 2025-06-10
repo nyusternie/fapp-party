f8df5d345e28c554816f1557951fedac6522d919
Shomari <nyusternie@sdot.io>
2025-06-10 18:02:28 -0400

    Update transparency page.

--- src/components/miniapp/Transparency.vue
+++ src/components/miniapp/Transparency.vue
@@ -1,13 +1,46 @@
 <template>
     <main class="w-full h-full px-2 pb-3 flex flex-col gap-6 overflow-y-scroll">
-		<h1 class="mt-5 text-pretty text-5xl font-semibold tracking-tight text-amber-200 uppercase">
-			Transparency
-		</h1>
+        <div class="py-6">
+            <div class="mx-auto max-w-2xl px-4">
+                <div class="grid grid-cols-1 items-center gap-x-16 gap-y-10">
+                    <div>
+                        <h2 class="text-4xl font-bold tracking-tight text-slate-200">
+							100% Transparency
+						</h2>
 
-		<p class="text-slate-300 font-medium text-lg">
-			Learn about everything going on behind the scenes before, during and after the Mini Apps Party.
-		</p>
-	</main>
+						<p class="mt-4 text-stone-300">
+							At the beggining of this journey, I set out to create an OPEN community for BUILDERS!
+							As much as possible, transparency is though of FIRST when designing the systems that power this community.
+                        </p>
+                    </div>
+
+					<img
+						src="https://images.unsplash.com/photo-1512099053734-e6767b535838?q=80"
+						alt=""
+						class="aspect-[2/1] w-full rounded-lg bg-slate-100 object-cover"
+					/>
+                </div>
+
+				<div class="mt-16 grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-3">
+                    <div v-for="incentive in incentives" :key="incentive.name" class="sm:flex lg:block">
+                        <div class="sm:shrink-0">
+                            <img class="p-2 size-16 bg-stone-300 rounded-full" :src="incentive.imageSrc" alt="" />
+                        </div>
+
+						<div class="mt-4 sm:ml-6 sm:mt-0 lg:ml-0 lg:mt-6">
+                            <h3 class="text-xl font-medium text-slate-200 tracking-wider">
+								{{ incentive.name }}
+							</h3>
+
+							<p class="mt-2 text-base/7 text-stone-300">
+								{{ incentive.description }}
+							</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
 </template>
 
 <script setup lang="ts">
@@ -26,6 +59,24 @@ const props = defineProps({
 })
 
 const System = useStore($System)
+
+const incentives = [
+	{
+		name: `Verified Smart Contracts`,
+		imageSrc: 'https://tailwindcss.com/plus-assets/img/ecommerce/icons/icon-warranty-simple.svg',
+		description: `All of the smart contracts are verified on Basescan—with FULL audits planned for -as soon as economically possible.`,
+	},
+	{
+		name: `10-year warranty`,
+		imageSrc: 'https://tailwindcss.com/plus-assets/img/ecommerce/icons/icon-warranty-simple.svg',
+		description: `If it breaks in the first 10 years we'll replace it. After that you're on your own though.`,
+	},
+	{
+		name: `Exchanges`,
+		imageSrc: 'https://tailwindcss.com/plus-assets/img/ecommerce/icons/icon-warranty-simple.svg',
+		description: `If you don't like it, trade it to one of your friends for something of theirs. Don't send it here though.`,
+	},
+]
 
 const init = async () => {
     console.log('SYSTEM', $System.get())
