f9738461567c24dd358dd81bb54556d34621533d
Shomari <nyusternie@sdot.io>
2025-06-10 19:10:04 -0400

    Fix button UI.

--- src/components/miniapp/AppDetail.vue
+++ src/components/miniapp/AppDetail.vue
@@ -115,13 +115,13 @@
             </div>
 
             <a :href="'/boost/' + appDetails.hostname" class="group cursor-pointer w-full mt-2 px-5 py-3 flex flex-col justify-center items-center bg-fuchsia-800 border-2 hover:bg-fuchsia-200 border-amber-900 rounded-xl">
-                <span class="inline-flex line-clamp-1 items-center gap-2 font-bold text-2xl text-center uppercase text-amber-300 group-hover:text-fuchsia-800">
+                <span class="inline-flex items-center gap-2 font-bold text-2xl text-center uppercase text-amber-300 group-hover:text-fuchsia-800">
                     Boost
                     <svg class="size-8 text-fuchsia-200 group-hover:text-fuchsia-800" data-slot="icon" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                         <path clip-rule="evenodd" fill-rule="evenodd" d="M4.606 12.97a.75.75 0 0 1-.134 1.051 2.494 2.494 0 0 0-.93 2.437 2.494 2.494 0 0 0 2.437-.93.75.75 0 1 1 1.186.918 3.995 3.995 0 0 1-4.482 1.332.75.75 0 0 1-.461-.461 3.994 3.994 0 0 1 1.332-4.482.75.75 0 0 1 1.052.134Z"></path>
                         <path clip-rule="evenodd" fill-rule="evenodd" d="M5.752 12A13.07 13.07 0 0 0 8 14.248v4.002c0 .414.336.75.75.75a5 5 0 0 0 4.797-6.414 12.984 12.984 0 0 0 5.45-10.848.75.75 0 0 0-.735-.735 12.984 12.984 0 0 0-10.849 5.45A5 5 0 0 0 1 11.25c.001.414.337.75.751.75h4.002ZM13 9a2 2 0 1 0 0-4 2 2 0 0 0 0 4Z"></path>
                     </svg>
-                    {{appDetails.appName}}
+                    <span class="line-clamp-1">{{appDetails.appName}}</span>
                 </span>
 
                 <small class="text-xs font-medium tracking-wider text-amber-400 group-hover:text-fuchsia-900">
