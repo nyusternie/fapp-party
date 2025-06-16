98a3452962d1f3108379114cc06d7400febcc794
Shomari <nyusternie@sdot.io>
2025-06-15 21:54:59 -0400

    Update search feature buttons.

--- src/components/miniapp/Search/SelectFeature.vue
+++ src/components/miniapp/Search/SelectFeature.vue
@@ -1,20 +1,20 @@
 <template>
     <main class="flex gap-2">
-        <button class="cursor-pointer inline-flex items-center gap-x-1.5 rounded-md px-2 py-1 text-lg font-medium text-stone-200 ring-1 ring-inset ring-gray-200">
+        <button class="cursor-pointer inline-flex items-center gap-x-1.5 rounded-md px-2 py-1 text-base font-medium text-stone-200 ring-1 ring-inset ring-gray-200">
             <svg class="size-3 fill-red-500" viewBox="0 0 6 6" aria-hidden="true">
                 <circle cx="3" cy="3" r="3" />
             </svg>
             BANGERS!
         </button>
 
-        <button class="cursor-pointer inline-flex items-center gap-x-1.5 rounded-md px-2 py-1 text-lg font-medium text-stone-200 ring-1 ring-inset ring-gray-200">
+        <button class="cursor-pointer inline-flex items-center gap-x-1.5 rounded-md px-2 py-1 text-base font-medium text-stone-200 ring-1 ring-inset ring-gray-200">
             <svg class="size-3 fill-yellow-500" viewBox="0 0 6 6" aria-hidden="true">
                 <circle cx="3" cy="3" r="3" />
             </svg>
             Fren Favs
         </button>
 
-        <button class="cursor-pointer inline-flex items-center gap-x-1.5 rounded-md px-2 py-1 text-lg font-medium text-stone-200 ring-1 ring-inset ring-gray-200">
+        <button class="cursor-pointer inline-flex items-center gap-x-1.5 rounded-md px-2 py-1 text-base font-medium text-stone-200 ring-1 ring-inset ring-gray-200">
             <svg class="size-3 fill-green-500" viewBox="0 0 6 6" aria-hidden="true">
                 <circle cx="3" cy="3" r="3" />
             </svg>
