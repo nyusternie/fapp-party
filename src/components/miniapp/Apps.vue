5cea32b6ceea8f977a412c563feff4c893ce05aa
Shomari <nyusternie@sdot.io>
2025-06-20 21:41:24 -0400

    Remove gap?

--- src/components/miniapp/Apps.vue
+++ src/components/miniapp/Apps.vue
@@ -1,8 +1,9 @@
 <template>
-    <main class="w-screen h-full px-2 pb-7 flex flex-col gap-6 overflow-y-scroll overflow-x-hidden">
+    <main class="w-screen h-full px-2 pb-7 flex flex-col overflow-y-scroll overflow-x-hidden">
+        <div class="bg-sky-300 w-screen h-4" />
         <div v-if="App && App.length > 0" class="w-screen">
             <div class="bg-green-300 w-full h-2" />
-            <div class="bg-green-300 w-screen h-4" />
+
             <!-- SPOTLIGHT MINI APPS-->
             <SpotlightCard :spotlight="spotlight" />
 
