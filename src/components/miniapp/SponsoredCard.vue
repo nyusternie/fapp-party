ff53285c26f8d368483210fc4c7c18186715629c
Shomari <nyusternie@sdot.io>
2025-06-20 22:02:35 -0400

    Test special cards.

--- src/components/miniapp/SponsoredCard.vue
+++ src/components/miniapp/SponsoredCard.vue
@@ -11,12 +11,12 @@
                 {{sponsored.tagline || sponsored.hostname}}
             </h3>
 
-            <section class="mt-3 isolate flex -space-x-2 overflow-hidden">
+            <!-- <section class="mt-3 isolate flex -space-x-2 overflow-hidden">
                 <img class="relative z-30 inline-block size-8 rounded-full ring-2 ring-white" src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
                 <img class="relative z-20 inline-block size-8 rounded-full ring-2 ring-white" src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
                 <img class="relative z-10 inline-block size-8 rounded-full ring-2 ring-white" src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=256&h=256&q=80" alt="" />
                 <img class="relative z-0 inline-block size-8 rounded-full ring-2 ring-white" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
-            </section>
+            </section> -->
         </div>
 
         <img v-if="sponsored.heroImageUrl" :src="sponsored.heroImageUrl" class="h-16 border border-slate-700 rounded-md" />
